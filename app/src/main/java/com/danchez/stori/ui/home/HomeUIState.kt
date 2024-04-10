fe2132f0b7fea65ff4519aa5d6cc98bc2e57eaeb
Daniel Sanchez <danielmsa96@gmail.com>
2024-04-10 12:44:00 -0500

    Appbar info in home screen

--- app/src/main/java/com/danchez/stori/ui/home/HomeUIState.kt
+++ app/src/main/java/com/danchez/stori/ui/home/HomeUIState.kt
@@ -1,16 +1,21 @@
 package com.danchez.stori.ui.home
 
+import android.net.Uri
+
 /**
  * Data class that represents the Home UI state
  */
 data class HomeUIState(
     val state: UIState = UIState.Initialized,
+    val displayName: String = "",
+    val photoUrl: Uri? = null,
 ) {
     sealed class UIState {
         data object Initialized : UIState()
         data object Success : UIState()
         data object Failure : UIState()
         data class ShowDetailsSheet(val transactionUIModel: TransactionUIModel) : UIState()
+        data object Logout : UIState()
         data object Loading : UIState()
     }
 }
